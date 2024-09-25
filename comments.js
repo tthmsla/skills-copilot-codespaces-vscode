53d34e8c68cc25814fdb47fb72586062da2b2722
tthmsla <44629581+tthmsla@users.noreply.github.com>
2024-09-25 19:55:45 +0000

    Copliot third commit

--- comments.js
+++ comments.js
@@ -12,3 +12,5 @@ app.get('/comments', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+// In comments.js, we have created a simple web server that listens on port 3000. When a request is made to http://localhost:3000/comments, the server responds with the text "Comments". This is a simple example of how to create a route in Express and send a response back to the client.
