d20ec3af8c36bc11237d6c7b0cc7560248ccca26
tthmsla <44629581+tthmsla@users.noreply.github.com>
2024-09-25 19:39:45 +0000

    Copliot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,14 @@
-// create web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// Create Web Server
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// create server
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+// Create a route
+app.get('/comments', (req, res) => {
+    res.send('Comments');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
